3e7444360e60be5437bbd1e5ce54842035aa2d2b
anshul-rohilla4 <anshulrohilla111@gmail.com>
2025-03-18 17:09:30 +0530

    Vite React application | movie exploration and trending search feature

---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+    return (
+        <div className="search"><div>
+            <img src="search.svg" alt='search' />
+            <input type='text'
+                placeholder='Search through movies'
+                value={searchTerm}
+                onChange={(event) => { setSearchTerm(event.target.value) }}>
+            </input>
+        </div>
+        </div>
+    )
+}
+
+export default Search
